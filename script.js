f3c6a13b9f0d5f60c7b7e8a8bfaf758c4c8e7c25
Saurav Singh <Sauravs1719@gmail.com>
2023-10-31 15:36:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+    const APIkey = 'b3e0b686a67b9d0a1c88cb941f30836f';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '') {
+        return;
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`)
+        .then(response => response.json())
+        .then(json => {
+
+            if (json.cod == '404') {
+                container.style.height = '450px';
+                weatherBox.classList.remove('active');
+                weatherDetails.classList.remove('active');
+                error404.classList.add('active');
+            } else {
+                container.style.height = '600px';
+                weatherBox.classList.add('active');
+                weatherDetails.classList.add('active');
+                error404.classList.remove('active');
+            }
+            
+
+            console.log(json.weather[0].main);
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+                case 'Mist':
+                    image.src = 'images/mist.png';
+                    break;
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+                default:
+                    image.src = 'images/cloud.png';
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.main.temp)}Km/h`;
+
+        });
+        
+});
